3339909bc7f30e51adf338aca22bceee49f7653e
[Ghost] <sardortohirov222@gmail.com>
2024-02-05 15:34:54 -0700

    Initial commit from Ghost !

--- src/main/java/com/example/publish_service/PublishServiceApplication.java
+++ src/main/java/com/example/publish_service/PublishServiceApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PublishServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PublishServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(PublishServiceApplication.class, args);
+    }
 
 }
--- src/test/java/com/example/publish_service/PublishServiceApplicationTests.java
+++ src/test/java/com/example/publish_service/PublishServiceApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class PublishServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
